1e0d62e5ccd334f147fd2bd60d1e7431c536dc63
Denis Forigo <denisforigo@gmail.com>
2019-07-25 00:09:49 -0300

    first commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'e9d4df448041f8b867ca29ef8f9c05ba',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'raja.db.elephantsql.com',
+  username: 'nctdmvlo',
+  database: 'nctdmvlo',
+  password: 'DhbmKWV2R_eeQbKuEzj8qsYLeC5SkFNU',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
