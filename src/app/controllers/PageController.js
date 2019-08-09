ebf910428c8675bd0e7b615c64655a25889c7b60
Denis Forigo <denisforigo@gmail.com>
2019-08-08 21:36:27 -0300

    deployng setup

--- src/app/controllers/PageController.js
+++ src/app/controllers/PageController.js
@@ -27,11 +27,14 @@ class PageController {
   }
 
   async index(req, res) {
-
     if (req.params.id) {
       const count = await Page.count();
       res.setHeader('X-Total-Count', count);
       res.setHeader('Access-Control-Expose-Headers', `X-Total-Count`);
+      res.setHeader(
+        'Access-Control-Allow-Origin',
+        `https://chun-front.herokuapp.com/`
+      );
       const cached = await Cache.get(`page+${req.params.id}`);
 
       if (cached) {
