2384e3c7ef232ed465ecc5a9a9b8859ffb5f396d
Denis Forigo <denisforigo@gmail.com>
2019-08-12 21:43:59 -0300

    Mailgun setup

---
+++ src/app/controllers/EmailController.js
@@ -0,0 +1,37 @@
+import Mailgun from 'mailgun-js';
+import creds from '../../config/email';
+
+class EmailController {
+  async send(req, res, next) {
+    const { name, email, message } = req.body;
+    const content = `name: ${name} \n email: ${email} \n message: ${message}`;
+    const domain = creds.USER;
+    const mailgun = new Mailgun({ apiKey: creds.MAILGUN_API_KEY, domain });
+    const data = {
+      // Specify email data
+      from: email,
+      // The email to contact
+      to: 'denisforigo@gmail.com',
+      // Subject and text data
+      subject: 'Mensagem enviada pelo Site',
+      html: content,
+    };
+    // Invokes the method to send emails given the above data with the helper library
+    mailgun.messages().send(data, (err, body) => {
+      // If there is an error, render the error page
+      if (err) {
+        res.status(404).json({ error: err });
+        console.log('got an error: ', err);
+      }
+      // Else we can greet    and leave
+      else {
+        // Here "submitted.jade" is the view file for this landing page
+        // We pass the variable "email" from the url parameter in an object rendered by Jade
+        res.status(200).json({ email });
+        console.log(body);
+      }
+    });
+  }
+}
+
+export default new EmailController();
--- src/routes.js
+++ src/routes.js
@@ -1,36 +1,17 @@
 import { Router } from 'express';
-import nodemailer from 'nodemailer';
 import multer from 'multer';
 import multerConfig from './config/multer';
-import creds from './config/email';
 
 import FileController from './app/controllers/FileController';
 import UserController from './app/controllers/UserController';
 import PageController from './app/controllers/PageController';
 import SessionController from './app/controllers/SessionController';
 import AlbumController from './app/controllers/AlbumController';
-
+import EmailController from './app/controllers/EmailController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
-
-const transporter = nodemailer.createTransport({
-  host: 'smtp.mailtrap.io',
-  port: 2525,
-  auth: {
-    user: creds.USER,
-    pass: creds.PASS,
-  },
-});
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take messages');
-  }
-});
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -58,26 +39,6 @@ routes.delete('/files/:id', FileController.delete);
 routes.delete('/files/all', FileController.delete_all);
 routes.post('/files', upload.single('file'), FileController.store);
 
-routes.post('/send', (req, res, next) => {
-  const { name, email, message } = req.body;
-  const content = `name: ${name} \n email: ${email} \n message: ${message}`;
-
-  const mail = {
-    from: name,
-    to: 'denisforigo@gmail.com',
-    subject: 'New Message from Contact Form',
-    text: content,
-  };
-
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({ msg: 'Houve algum erro no envio' });
-    } else {
-      res.json({
-        msg: 'Email enviado com sucesso! Entraremos em contato em breve.',
-      });
-    }
-  });
-});
+routes.post('/send', EmailController.send);
 
 export default routes;
