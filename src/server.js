478e9c3f2807165040043263352b7ba8a423f371
denisprado <denisforigo@gmail.com>
2019-10-09 10:29:31 -0300

    ENV variables

--- src/server.js
+++ src/server.js
@@ -1,4 +1,10 @@
 import app from './app';
 
-const port = process.env.PORT || 3333;
-app.listen(port);
+const port =
+  process.env.NODE_ENV === 'development'
+    ? process.env.LOCAL_PORT
+    : process.env.PORT;
+
+app.listen(port, function() {
+  console.log('Umbler listening on port %s', port);
+});
