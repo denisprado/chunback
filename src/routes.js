95895b96b969e9c1963183fe4bb17bd3a546bdce
Denis <denisforigo@gmail.com>
2019-10-22 23:50:55 -0300

    adding formData Capabilities

--- src/routes.js
+++ src/routes.js
@@ -41,8 +41,8 @@ routes.put('/users', authMiddleware, UserController.update);
 routes.delete('/files/:id', FileController.delete);
 routes.delete('/files/all', FileController.delete_all);
 // routes.post('/files', upload.single('file'), FileController.store);
-routes.post('/files', upload.single('file'), (req, res) => {
-  console.log(req.file);
+routes.post('/files', upload.array('file'), (req, res) => {
+  console.log(req.files);
   return null;
 });
 
