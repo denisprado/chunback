acdae842e69e706bc517c05f08c2e9b71d723e48
denisprado <denisforigo@gmail.com>
2019-10-24 14:40:41 -0300

    adding multiple upload capabilities

--- dist/routes.js
+++ dist/routes.js
@@ -40,8 +40,11 @@ routes.put('/users', _auth2.default, _UserController2.default.update);
 
 routes.delete('/files/:id', _FileController2.default.delete);
 routes.delete('/files/all', _FileController2.default.delete_all);
-// routes.post('/files', upload.single('file'), FileController.store);
-routes.post('/files', upload.single('files'), _FileController2.default.store);
+
+// routes.post('/files', upload.single('files'), FileController.store);
+routes.post('/files', upload.array('files'), req => {
+  console.log(req.files);
+}); // changed single to array to upload multiple files.
 
 routes.post('/send', _EmailController2.default.send);
 
--- src/routes.js
+++ src/routes.js
@@ -40,8 +40,11 @@ routes.put('/users', authMiddleware, UserController.update);
 
 routes.delete('/files/:id', FileController.delete);
 routes.delete('/files/all', FileController.delete_all);
-// routes.post('/files', upload.single('file'), FileController.store);
-routes.post('/files', upload.single('files'), FileController.store);
+
+// routes.post('/files', upload.single('files'), FileController.store);
+routes.post('/files', upload.array('files'), req => {
+  console.log(req.files);
+}); // changed single to array to upload multiple files.
 
 routes.post('/send', EmailController.send);
 
