1e19e770ce334f01f54e670b626db703705ebb6a
denisprado <denisforigo@gmail.com>
2019-10-24 14:50:32 -0300

    console log res.json(req.files

--- dist/routes.js
+++ dist/routes.js
@@ -43,7 +43,7 @@ routes.delete('/files/all', _FileController2.default.delete_all);
 
 // routes.post('/files', upload.single('files'), FileController.store);
 routes.post('/files', upload.array('files'), (req, res) => {
-  console.log(res.json(req.files));
+  console.log(req.files);
   return null;
 }); // changed single to array to upload multiple files.
 
