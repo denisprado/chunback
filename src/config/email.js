2772a21439d01a7447bd4c20e230c39b3f3ca93e
Denis <denisforigo@gmail.com>
2019-10-24 23:33:40 -0300

    adding mailgun

--- src/config/email.js
+++ src/config/email.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MAILGUN_USER: process.env.USER,
-  MAILGUN_PASS: process.env.PASS,
+  MAILGUN_USER: process.env.MAILGUN_USER,
+  MAILGUN_PASS: process.env.MAILGUN_PASS,
   MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
 };
